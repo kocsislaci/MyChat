c71ebf7c11c25c92dc9c19dfda2e22920403f6e9
kocsislaci <kocsislaci95@gmail.com>
2020-05-28 18:16:04 +0200

    hf

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Login } from './Login'
+import { Main } from './Main'
+import { proxy } from './Proxy';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component
+{
+  state = {loggedIn: false}
+  render()
+  {
+    return (
+      <div className="app">
+        {this.state.loggedIn ? <Main />:<Login />}
+      </div>
+    );
+  }
+  componentDidMount(){
+    proxy.addEventListener("login", () =>{
+      this.setState({loggedIn: true});
+    }, this);
+  }
+
+  componentWillUnmount(){
+    proxy.removeAllEventListener(this);
+  }
 }
-
-export default App;
