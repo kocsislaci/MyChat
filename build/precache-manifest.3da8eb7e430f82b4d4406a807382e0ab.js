f2f52cec137e32ca654a8b96c764e56e70d944ad
kocsislaci <kocsislaci95@gmail.com>
2020-05-28 18:18:14 +0200

    hf1

---
+++ build/precache-manifest.3da8eb7e430f82b4d4406a807382e0ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cae7257e77517f05123a378b543dc7b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2686a8f5f5f51baa14c",
+    "url": "./static/css/main.668bb3e1.chunk.css"
+  },
+  {
+    "revision": "07c155597dc075058377",
+    "url": "./static/js/2.821ad3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.821ad3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2686a8f5f5f51baa14c",
+    "url": "./static/js/main.49e02d48.chunk.js"
+  },
+  {
+    "revision": "763f551d24bf50b1938c",
+    "url": "./static/js/runtime-main.c7b6dbd4.js"
+  }
+]);
